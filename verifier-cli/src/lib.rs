2b88a0323f598c5a46e8307a57aa7397c5ec1f48
Philip Tricca <flihp@twobit.org>
2025-04-16 13:50:30 -0700

    wip: Add IPCC implementation for `Attester` trait.

--- verifier-cli/src/lib.rs
+++ verifier-cli/src/lib.rs
@@ -3,6 +3,12 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 pub mod sprot;
+use sprot::AttestHiffy;
+
+#[cfg(feature = "ipcc")]
+pub mod ipcc;
+#[cfg(feature = "ipcc")]
+use ipcc::Ipcc;
 
 use anyhow::Result;
 use attest_data::{Attestation, Log, Nonce};
@@ -16,9 +22,24 @@ pub trait Attester {
     fn attest(&self, nonce: &Nonce) -> Result<Attestation>;
 }
 
+impl dyn Attester {
+    pub fn new(interface: Interface) -> Result<Box<dyn Attester>> {
+        match interface {
+            #[cfg(feature = "ipcc")]
+            Interface::Ipcc => Ok(Box::new(Ipcc::new()?)),
+            Interface::Rot | Interface::Sprot => {
+                Ok(Box::new(AttestHiffy::new(interface)))
+            }
+        }
+    }
+}
+// TODO: Create factory type that creates an `Attester` from an instance of
+// the  `Interface` enum.
 /// An enum of the possible routes to the `Attest` task.
 #[derive(Clone, Debug, ValueEnum)]
 pub enum Interface {
+    #[cfg(feature = "ipcc")]
+    Ipcc,
     Rot,
     Sprot,
 }
@@ -26,6 +47,8 @@ pub enum Interface {
 impl fmt::Display for Interface {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
+            #[cfg(feature = "ipcc")]
+            Interface::Ipcc => write!(f, "Ipcc"),
             Interface::Rot => write!(f, "Attest"),
             Interface::Sprot => write!(f, "SpRot"),
         }
