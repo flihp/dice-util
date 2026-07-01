b7abcfe5d39b1231a3b8e47c4ab0ab95856ed9d0
Philip Tricca <flihp@twobit.org>
2026-07-01 15:32:52 -0700

    wip
    uses pki-playground from dev branch, dependency stuff between
    pki-playground and omicron / sledagent

--- verifier/build.rs
+++ verifier/build.rs
@@ -2,20 +2,32 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use anyhow::Result;
-use anyhow::{anyhow, Context};
+use camino::Utf8PathBuf;
+use miette::{IntoDiagnostic, Result, WrapErr};
+
+use std::env;
 
 fn main() -> Result<()> {
     // output directory where we put:
     // generated test inputs
-    let mut out =
-        PathBuf::from(env::var("OUT_DIR").context("Could not get OUT_DIR")?);
+    let out = Utf8PathBuf::from(
+        env::var("OUT_DIR")
+            .into_diagnostic()
+            .wrap_err("Could not get OUT_DIR")?,
+    );
 
-    let config_path = "config.kdl";
-    let doc = pki_playground::config::load_and_validate(&config_path)
-        .wrap_err(format!("Loading config from \"{}\" failed", config_path))?;
+    let config_path = "test-pki.kdl";
+    let doc = pki_playground::config::load_and_validate(config_path.as_ref())
+        .wrap_err(format!(
+        "Loading config from \"{}\" failed",
+        config_path
+    ))?;
 
-    doc.write_key_pairs(out, action_opts.output_exists)?;
-    doc.write_certificates(out, action_opts.output_exists)
-    doc.write_certificate_lists(out, action_opts.output_exists)?;
+    let behavior = pki_playground::OutputFileExistsBehavior::Skip;
+
+    doc.write_key_pairs(out.clone(), behavior)?;
+    doc.write_certificates(out.clone(), behavior)?;
+    doc.write_certificate_lists(out.clone(), behavior)?;
+
+    Ok(())
 }
